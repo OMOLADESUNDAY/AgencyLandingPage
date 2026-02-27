6e73428ea2c41f47421715e9b31770284bacd0ac
OMOLADESUNDAY <sundayomoladee11@gmail.com>
2026-02-27 10:37:12 +0100

    Navbar logic

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,31 +5,41 @@ import { Link } from "react-router-dom";
 import "./navbar.css"
 const Navbar = () => {
   const [navOpen,setNavOpen]=useState(false)
+  const [showNav,setShowNav]=useState('')
   const MenuToggler=()=>{
     setNavOpen((prev)=>!prev)
+    if(navOpen){
+      setShowNav('block')
+      console.log('block')
+    }
+    else{
+      setShowNav('hidden')
+      console.log('hidden')
+    }
   }
   return (
-    <div className="container flex items-start my-5 justify-between m-auto relative">
+    <div className="container flex items-start my-5 justify-between m-auto relative md:items-center">
       <div className="flex gap-2 items-center "><span className="bg-gradient-to-r from-[#1b72e8] to-[#4831c7] JT-logo">JT</span><span>Solutions</span></div>
       {/* <img src={logo} alt="logo" width="128.51px" height="48px" className=" border border-red-600" /> */}
-      <nav className="flex items-center absolute  left-1/2 -translate-x-1/2 hidden md:block ">
+      <nav className={navOpen?"flex items-center absolute  left-1/2 -translate-x-1/2 flex-col md:flex-row":"flex items-center absolute  left-1/2 -translate-x-1/2 flex-col hidden md:flex-row"}>
         <div className="menu">
           <ul className="flex flex-col items-center gap-[25px] md:flex-row">
               <li><Link>Our Story</Link></li>
               <li><Link>About Us</Link></li>
               <li><Link>Services</Link></li>
               <li><Link>Contact Us</Link></li>
-              <button className="flex items-center btn-primary ">
-                <span>Get Started</span><span className="bg-white rounded-full w-[26px] ml-[5px]"><MoveRight className="text-black w-[18px] m-auto" /></span>
-              </button>
+              
           </ul>
         </div>
+        <button className="flex items-center btn-primary md:hidden">
+                <span>Get Started</span><span className="bg-white rounded-full w-[26px] ml-[5px]"><MoveRight className="text-black w-[18px] m-auto" /></span>
+        </button>
        
       </nav>
-      <button className="flex items-center btn-primary hidden">
+      <button className="flex items-center btn-primary hidden md:flex">
           <span>Get Started</span><span className="bg-white rounded-full w-[26px] ml-[5px]"><MoveRight className="text-black w-[18px] m-auto" /></span>
         </button>
-        {navOpen?<X className=" cursor-pointer" onClick={()=>MenuToggler()}/>:<Menu className="cursor-pointer" onClick={()=>MenuToggler()}/>}
+        {navOpen?<X className=" cursor-pointer md:hidden z-20" onClick={()=>MenuToggler()}/>:<Menu className="cursor-pointer md:hidden z-20" onClick={()=>MenuToggler()}/>}
     </div>
   );
 };
